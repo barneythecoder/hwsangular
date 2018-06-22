e778525e8166bbf6757ad96b45a102f1baddbe96
barneythecoder <rclaudeismael@gmail.com>
2018-06-22 10:09:45 +0800

    Treats

--- ng-src/src/app/components/treats/treats.component.ts
+++ ng-src/src/app/components/treats/treats.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MatDialogConfig } from '@angular/material';
+import { OrderFormComponent } from '../order-form/order-form.component';
 
 @Component({
   selector: 'app-treats',
@@ -7,7 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TreatsComponent implements OnInit {
 
-  constructor() { }
+  orderFormDialogRef: MatDialogRef<OrderFormComponent>;
+  orderData: {}
+  constructor(private dialog: MatDialog) { }
+  openOrderFormDialog(items){
+    let config: MatDialogConfig={
+      width: '',
+      height: '',
+      position: {
+        top: '',
+        bottom: '',
+        left: '',
+        right: ''
+      },
+      data: {
+        itemName: items.itemName,
+        itemPrice: items.itemPrice
+      }
+    }
+    this.orderFormDialogRef = this.dialog.open(OrderFormComponent, config);
+
+    this.orderFormDialogRef
+    .afterClosed()
+    .pipe(order => order)
+    .subscribe(order => {      
+      
+     
+
+    });
+
+    
+  }
 
   ngOnInit() {
   }
